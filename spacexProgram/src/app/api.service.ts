d0aa53367dde9aad3510df627a09d58ac06309ac
Rajsree <shanrajsree@gmail.com>
2020-08-25 14:52:57 -0600

    Service added

--- spacexProgram/src/app/api.service.ts
+++ spacexProgram/src/app/api.service.ts
@@ -6,19 +6,31 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ApiService {
 
-// private SERVER_URL = "https://api.spacexdata.com/v3/launches?limit=100";
-//   setGroupFilter$ = new Subject<any>();
-//   	getGroupFilter = this.setGroupFilter$.asObservable();
-//   	constructor(private httpClient: HttpClient) {}
-//   	fetchUsers(): Observable<any> {
-//   		return of(USERS);
-//   	}
 
-  private SERVER_URL = "https://api.spacexdata.com/v3/launches?limit=100";
+  //setGroupFilter$ = new Subject<any>();
+  	//getGroupFilter = this.setGroupFilter$.asObservable();
 
- constructor(private httpClient: HttpClient) { }
+    filterObj = {
+      land_success : "",
+      launch_success :"",
+      launch_year : ""
+    };
+
+    private SERVER_URL = "https://api.spacexdata.com/v3/launches?limit=100&amp;launch_success="+this.filterObj.launch_success+"&amp;land_success="+this.filterObj.land_success+"&amp;launch_year="+this.filterObj.launch_year;
+
+  	constructor(private httpClient: HttpClient) {}
+
+
+  	// fetchUsers(): Observable<any> {
+  	// 	return of(USERS);
+  	// }
+
+ //  private SERVER_URL = "https://api.spacexdata.com/v3/launches?limit=100";
+ //
+ // constructor(private httpClient: HttpClient) { }
 
   public get(){
+    //  console.log('Analysing :' + getGroupFilter);
 		return this.httpClient.get(this.SERVER_URL);
 	}
 }
--- spacexProgram/src/app/filter/filter.component.ts
+++ spacexProgram/src/app/filter/filter.component.ts
@@ -16,9 +16,7 @@ export class FilterComponent implements OnInit {
   	searchText: string = '';
   	constructor(private fb: FormBuilder,  	private apiService: ApiService) {}
 
-    land_success = '';
-    launch_success = '';
-    launch_year="";
+    filterObj = ApiService.filterObj;
 
   	ngOnInit(): void {
   		this.buildForm();
@@ -35,26 +33,27 @@ export class FilterComponent implements OnInit {
 
     set_launch_year(year): void {
       console.log("Lunch Year : ", year);
-      this.launch_year = year;
-      console.log("Current status : Launch Year -> : " + this.launch_year + "Launch_success -> :"+this.launch_success+ "Land -> :" +this.land_success)
+      console.log("Filter in filterObj :", this.apiService.filterObj);
+      this.apiService.filterObj.launch_year = year;
+      //console.log("Current status : Launch Year -> : " + this.launch_year + "Launch_success -> :"+this.launch_success+ "Land -> :" +this.land_success)
     }
     set_launch_success(event): void {
       console.log("Lunch Success : ", event);
-      this.launch_success = event.target.outerText;
-      console.log("Current status : Launch Year -> : " + this.launch_year + "Launch_success -> :"+this.launch_success+ "Land -> :" +this.land_success)
+      this.apiService.filterObj.launch_success = event.target.outerText;
+      console.log("Filter in filterObj :", this.apiService.filterObj);
     }
 
     set_land_success(event): void {
       console.log("Land Success : ", event.value);
-      this.land_success = event.value;
-      console.log("Current status : Launch Year -> : " + this.launch_year + "Launch_success -> :"+this.launch_success+ "Land -> :" +this.land_success)
+      this.apiService.filterObj.land_success = event.value;
+      console.log("Filter in filterObj :", this.apiService.filterObj);
     }
 
-    mycheck(filters): void {
-      console.log("FILETER SS :: ",filters)
-  	   Object.keys(filters).forEach(key => {
-         console.log("KEY   :",key);
-         filters[key] === '' ? delete filters[key] : key});
-  	    this.groupFilters.emit(filters);
-      }
+    // mycheck(filters): void {
+    //   console.log("FILETER SS :: ",filters)
+  	//    Object.keys(filters).forEach(key => {
+    //      console.log("KEY   :",key);
+    //      filters[key] === '' ? delete filters[key] : key});
+  	//     this.groupFilters.emit(filters);
+    //   }
   	}
