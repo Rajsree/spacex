4fafd48cc30c20290225dfe9854a175787c16b3f
RajSree <shanrajsree@gmail.com>
2020-08-27 15:36:54 -0600

    Folder structure change

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/dist/spacex-program'));
+
+/*
+app.get('/', (req, res) => {
+    res.send('An alligator approaches!');
+});*/
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/spacex-program/index.html'));
+});
+
+app.listen(process.env.PORT || 8080, () => console.log('Gator app listening on port 3000!'));
