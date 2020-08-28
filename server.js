9e8fd2d51f241c2e9cfb256b90536e6becb23d24
RajSree <shanrajsree@gmail.com>
2020-08-27 23:03:46 -0600

    Updates to deploy to Heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.static(__dirname + '/dist/spacex-program'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/spacex-program/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, () => console.log('Gator app listening on port 8080!'));
